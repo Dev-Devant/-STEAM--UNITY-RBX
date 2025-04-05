abe1181f8c5b58975819853f377ea34c1827df00
Dev-Devant <andeliros@yahoo.com.ar>
2025-04-05 12:28:53 -0300

    primer brak

--- Assets/scripts/GUIController.cs
+++ Assets/scripts/GUIController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class GUIController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GUIController : MonoBehaviour{
+    
+
+    private Stats playerStats;
+    public TextMeshProUGUI healthDisplay;
+    
+    void Start()    {
+        GameObject player = GameObject.Find("Player");
+        if (player == null){
+            Debug.LogError("GUI FUERA DE LINEA. No puso un player valido ( Debe llamarse Player). por favor programa bien!");
+            return;
+        }
+
+        playerStats = player.GetComponent<Stats>();
+        healthDisplay.text = "AAAAAAAA";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update()    {
+        if (playerStats == null){
+            Debug.LogError("GUI FUERA DE LINEA. El jugador no posee estadisticas compatibles");
+            return;
+        }
     }
+
+
 }
