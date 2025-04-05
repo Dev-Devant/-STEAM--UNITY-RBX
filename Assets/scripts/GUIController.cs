8eb484fed5bf050cb76ccfde658cd7a79533acd4
Dev-Devant <andeliros@yahoo.com.ar>
2025-04-05 13:29:50 -0300

    aaaaaaaaaaa

--- Assets/scripts/GUIController.cs
+++ Assets/scripts/GUIController.cs
@@ -2,20 +2,16 @@ using UnityEngine;
 using TMPro;
 
 public class GUIController : MonoBehaviour{
-    
-
     private Stats playerStats;
     public TextMeshProUGUI healthDisplay;
-    
+    public RectTransform healthBar ;
     void Start()    {
         GameObject player = GameObject.Find("Player");
         if (player == null){
             Debug.LogError("GUI FUERA DE LINEA. No puso un player valido ( Debe llamarse Player). por favor programa bien!");
             return;
         }
-
         playerStats = player.GetComponent<Stats>();
-        healthDisplay.text = "AAAAAAAA";
     }
 
     void Update()    {
@@ -23,7 +19,8 @@ public class GUIController : MonoBehaviour{
             Debug.LogError("GUI FUERA DE LINEA. El jugador no posee estadisticas compatibles");
             return;
         }
+        healthDisplay.text = "Vida: " + (int)playerStats.health;
+        float percent = playerStats.health/playerStats.healthMax;
+        healthBar.localScale = new Vector3 (percent,1,0);
     }
-
-
 }
